89b5b3c0ea8ad4e05326f9bc1fca16a50a5f5b75
faxtork <sebastian.esparza@ceinf.cl>
2014-05-21 17:27:59 -0400

    barra de navegacion

--- application/views/general/headers.php
+++ application/views/general/headers.php
@@ -2,15 +2,20 @@
 <!DOCTYPE html> 
 <html>
     <head>
-        <title></title>
-        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+<meta http-equiv="content-type" content="text/html; charset=UTF-8">
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
         <!-- Bootstrap -->
         <link href="<?php echo base_url()?>public/bootstrap/css/bootstrap.min.css" rel="stylesheet" type="text/css" media="screen">
-                <link href="<?php echo base_url()?>public/bootstrap/css/bootstrap.css" rel="stylesheet" type="text/css" media="screen">
+        <link href="<?php echo base_url()?>public/bootstrap/css/bootstrap.css" rel="stylesheet" type="text/css" media="screen">
+        <link href="<?php echo base_url()?>public/bootstrap/css/bootstrap-responsive.css" rel="stylesheet" type="text/css" media="screen">
 
         <link href="<?php echo base_url()?>public/css/style_general.css" rel="stylesheet" type="text/css" media="screen">
         <script src="<?php echo base_url()?>public/js/jquery.min.js" type="text/javascript"></script>
         <script src="<?php echo base_url()?>public/js/jquery.js" type="text/javascript"></script>
         <script type="text/javascript" src="<?php echo base_url() ?>public/js/funciones.js"></script>
+        <script type="text/javascript" src="<?php echo base_url() ?>public/js/bootstrap-responsive.js"></script>
+      
     </style>
     </head>
--- application/views/general/menu_principal.php
+++ application/views/general/menu_principal.php
@@ -1,27 +1,33 @@
-   
-    <body >
- <script src="http://code.jquery.com/jquery.js"></script>
-        <script src="js/bootstrap.min.js"></script>
-  <h3 class="muted">Horario de Clases</h3>
-        
-      <div class="container">
-        <div class="navbar-header">
-          <button class="navbar-toggle" type="button" data-toggle="collapse" data-target="#navbar-main">
-            <span class="icon-bar"></span>
-            <span class="icon-bar"></span>
-            <span class="icon-bar"></span>
-          </button>
-        </div>
-        <div class="navbar-collapse collapse" id="navbar-main">
-          <ul class="nav navbar-nav">
-            <li><a href="<?= site_url('consulta');?>">Consulta</a></li>
-            <li><a href="<?= site_url('pedidos');?>">Pedidos</a></li>
-            <li><a href="<?= site_url('intranet');?>">Intranet</a></li>
-            <li><a  href="<?= site_url('contacto');?>">Contacto</a></li>
-          </ul>
-                <?php if(isset($_SESSION['bnv']) || isset($_SESSION['usuarioProfesor']) || isset($_SESSION['usuarioAdmin']))
-                { 
-                  ?><ul class="nav pull-right"><li ><button class="btn btn-warning" onclick="location.href='<?= site_url('login/desconectar') ?>'" >Desconectar</button></li></ul><?php
-                }?>
-        </div>
-      </div>
+
+
+<nav class="navbar navbar-default" role="navigation">
+  <div class="container-fluid">
+    <!-- Brand and toggle get grouped for better mobile display -->
+    <div class="navbar-header">
+      <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+        <span class="sr-only">Toggle navigation</span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+      </button>
+      <a class="navbar-brand" href="#">Horario de Clases</a>
+    </div>
+
+    <!-- Collect the nav links, forms, and other content for toggling -->
+    <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+      <ul class="nav navbar-nav">
+        <li><a href="<?= site_url('consulta');?>">Consulta</a></li>
+        <li><a href="<?= site_url('pedidos');?>">Pedidos</a></li>
+        <li><a href="<?= site_url('intranet');?>">Intranet</a></li>
+        <li><a  href="<?= site_url('contacto');?>">Contacto</a></li>
+      </ul>
+    </div><!-- /.navbar-collapse -->
+  </div><!-- /.container-fluid -->
+</nav>
+
+
+
+
+            <script src="http://code.jquery.com/jquery.js"></script>
+                    <script type="text/javascript" src="<?php echo base_url() ?>../public/js/bootstrap-collapse.js"></script>
+       
