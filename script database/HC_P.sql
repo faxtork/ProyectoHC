f76ae6bf712d7c3fcea188ec2be3ac39800f3e69
faxtork <sebastian.esparza@ceinf.cl>
2014-06-06 01:15:34 -0400

    imagen de la nuevas relaciones (+2 tablas)

--- script database/HC_P.sql
+++ script database/HC_P.sql
@@ -101,6 +101,55 @@ ALTER SEQUENCE asignaturas_pk_seq OWNED BY asignaturas.pk;
 
 
 --
+-- Name: asistencia; Type: TABLE; Schema: public; Owner: postgres; Tablespace: 
+--
+
+CREATE TABLE asistencia (
+    pk bigint NOT NULL,
+    firma integer,
+    fecha date DEFAULT now() NOT NULL,
+    docente_fk integer NOT NULL
+);
+
+
+ALTER TABLE public.asistencia OWNER TO postgres;
+
+--
+-- Name: bitacora; Type: TABLE; Schema: public; Owner: postgres; Tablespace: 
+--
+
+CREATE TABLE bitacora (
+    pk integer NOT NULL,
+    ip character varying NOT NULL,
+    fecha date DEFAULT now() NOT NULL,
+    administrador_fk bigint NOT NULL
+);
+
+
+ALTER TABLE public.bitacora OWNER TO postgres;
+
+--
+-- Name: bitacora_pk_seq; Type: SEQUENCE; Schema: public; Owner: postgres
+--
+
+CREATE SEQUENCE bitacora_pk_seq
+    START WITH 1
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1;
+
+
+ALTER TABLE public.bitacora_pk_seq OWNER TO postgres;
+
+--
+-- Name: bitacora_pk_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: postgres
+--
+
+ALTER SEQUENCE bitacora_pk_seq OWNED BY bitacora.pk;
+
+
+--
 -- Name: cursos; Type: TABLE; Schema: public; Owner: postgres; Tablespace: 
 --
 
@@ -402,6 +451,13 @@ ALTER TABLE ONLY asignaturas ALTER COLUMN pk SET DEFAULT nextval('asignaturas_pk
 -- Name: pk; Type: DEFAULT; Schema: public; Owner: postgres
 --
 
+ALTER TABLE ONLY bitacora ALTER COLUMN pk SET DEFAULT nextval('bitacora_pk_seq'::regclass);
+
+
+--
+-- Name: pk; Type: DEFAULT; Schema: public; Owner: postgres
+--
+
 ALTER TABLE ONLY cursos ALTER COLUMN pk SET DEFAULT nextval('cursos_pk_seq'::regclass);
 
 
@@ -458,9 +514,11 @@ ALTER TABLE ONLY salas ALTER COLUMN pk SET DEFAULT nextval('salas_pk_seq'::regcl
 -- Data for Name: administrador; Type: TABLE DATA; Schema: public; Owner: postgres
 --
 
-INSERT INTO administrador VALUES (1, 'jose', '4321', 'jose@utem.cl', '21776304-5');
-INSERT INTO administrador VALUES (31, 'sebastian', 'nose', 'nose@jaja', 'nose');
-INSERT INTO administrador VALUES (30, 'juancho2.0', 'nose', 'nose@jaja.com', 'nosexd');
+COPY administrador (pk, nombre, clave, contacto, rut) FROM stdin;
+1	jose	4321	jose@utem.cl	21776304-5
+31	sebastian	nose	nose@jaja	nose
+30	juancho2.0	nose	nose@jaja.com	nosexd
+\.
 
 
 --
@@ -474,17 +532,19 @@ SELECT pg_catalog.setval('administrador_pk_seq', 32, true);
 -- Data for Name: asignaturas; Type: TABLE DATA; Schema: public; Owner: postgres
 --
 
-INSERT INTO asignaturas VALUES (104, 1, 'INF-762', 'Computacion Paralela', '');
-INSERT INTO asignaturas VALUES (93, 1, 'INF-648', 'Analisis de Algoritmo', '');
-INSERT INTO asignaturas VALUES (34, 1, 'INF-626', 'Lenguaje de Expresiones', '');
-INSERT INTO asignaturas VALUES (55, 1, 'INF-642', 'Lenguaje de Programacion', '');
-INSERT INTO asignaturas VALUES (111, 1, 'INF-752', 'Gestion Financiera de TI', '');
-INSERT INTO asignaturas VALUES (82, 1, 'INF-653', 'Simulacion de Sistemas', '');
-INSERT INTO asignaturas VALUES (103, 1, 'INF-750', 'Optimizacion de Sistemas', '');
-INSERT INTO asignaturas VALUES (73, 1, 'INF-644', 'Teorias Automatas', '');
-INSERT INTO asignaturas VALUES (1, 1, 'EFE', 'Computacion Movil', '');
-INSERT INTO asignaturas VALUES (105, 1, 'INF-658', 'Auditoria de Sistemas', '');
-INSERT INTO asignaturas VALUES (65, 1, 'INF-631', 'Bases de Datos', '');
+COPY asignaturas (pk, departamento_fk, codigo, nombre, descripcion) FROM stdin;
+104	1	INF-762	Computacion Paralela	
+93	1	INF-648	Analisis de Algoritmo	
+34	1	INF-626	Lenguaje de Expresiones	
+55	1	INF-642	Lenguaje de Programacion	
+111	1	INF-752	Gestion Financiera de TI	
+82	1	INF-653	Simulacion de Sistemas	
+103	1	INF-750	Optimizacion de Sistemas	
+73	1	INF-644	Teorias Automatas	
+1	1	EFE	Computacion Movil	
+105	1	INF-658	Auditoria de Sistemas	
+65	1	INF-631	Bases de Datos	
+\.
 
 
 --
@@ -495,10 +555,35 @@ SELECT pg_catalog.setval('asignaturas_pk_seq', 1, false);
 
 
 --
+-- Data for Name: asistencia; Type: TABLE DATA; Schema: public; Owner: postgres
+--
+
+COPY asistencia (pk, firma, fecha, docente_fk) FROM stdin;
+\.
+
+
+--
+-- Data for Name: bitacora; Type: TABLE DATA; Schema: public; Owner: postgres
+--
+
+COPY bitacora (pk, ip, fecha, administrador_fk) FROM stdin;
+\.
+
+
+--
+-- Name: bitacora_pk_seq; Type: SEQUENCE SET; Schema: public; Owner: postgres
+--
+
+SELECT pg_catalog.setval('bitacora_pk_seq', 1, false);
+
+
+--
 -- Data for Name: cursos; Type: TABLE DATA; Schema: public; Owner: postgres
 --
 
-INSERT INTO cursos VALUES (1, 2, 2013, 82, 12, 1);
+COPY cursos (pk, semestre, anio, asignatura_fk, docente_fk, seccion) FROM stdin;
+1	2	2013	82	12	1
+\.
 
 
 --
@@ -512,8 +597,10 @@ SELECT pg_catalog.setval('cursos_pk_seq', 1, true);
 -- Data for Name: departamentos; Type: TABLE DATA; Schema: public; Owner: postgres
 --
 
-INSERT INTO departamentos VALUES (2, 1, 'Industria', '');
-INSERT INTO departamentos VALUES (1, 1, 'Informatica', '');
+COPY departamentos (pk, facultad_fk, departamento, descripcion) FROM stdin;
+2	1	Industria	
+1	1	Informatica	
+\.
 
 
 --
@@ -527,23 +614,25 @@ SELECT pg_catalog.setval('departamentos_pk_seq', 6, true);
 -- Data for Name: docentes; Type: TABLE DATA; Schema: public; Owner: postgres
 --
 
-INSERT INTO docentes VALUES (1, 'Mauro', 'Castillo Valdes', '001', 1);
-INSERT INTO docentes VALUES (2, 'Francisco Alberto ', 'Cofré Gajardo', '002', 1);
-INSERT INTO docentes VALUES (3, 'Ricardo Osvaldo', 'Corbinaud Perez', '003', 1);
-INSERT INTO docentes VALUES (4, 'Victor Heughes', 'Escobar Jeria', '004', 1);
-INSERT INTO docentes VALUES (5, 'Oscar', 'Magna Veloso', '005', 1);
-INSERT INTO docentes VALUES (6, 'Patricia', 'Mellado Acevedo', '006', 1);
-INSERT INTO docentes VALUES (7, 'Rene', 'Peña Aguilar', '007', 1);
-INSERT INTO docentes VALUES (8, 'Héctor Manuel', 'Pincheira Conejeros', '008', 1);
-INSERT INTO docentes VALUES (9, 'Sara', 'Rojas Aldea', '009', 1);
-INSERT INTO docentes VALUES (10, 'Marta', 'Rojas Estay', '010', 1);
-INSERT INTO docentes VALUES (11, 'Maria Victoria', 'Vallejos Amado', '011', 1);
-INSERT INTO docentes VALUES (12, 'Santiago', 'Zapata Caceres', '012', 1);
-INSERT INTO docentes VALUES (13, 'Alejandro', 'Reyes', '013', 1);
-INSERT INTO docentes VALUES (14, 'Sergio', 'Muñoz', '014', 1);
-INSERT INTO docentes VALUES (15, 'Jorge', 'Pavez', '015', 1);
-INSERT INTO docentes VALUES (16, 'Jorge', 'Morris', '016', 1);
-INSERT INTO docentes VALUES (17, 'Rene', 'Peña', '017', 1);
+COPY docentes (pk, nombres, apellidos, rut, departamento_fk) FROM stdin;
+1	Mauro	Castillo Valdes	001	1
+2	Francisco Alberto 	Cofré Gajardo	002	1
+3	Ricardo Osvaldo	Corbinaud Perez	003	1
+4	Victor Heughes	Escobar Jeria	004	1
+5	Oscar	Magna Veloso	005	1
+6	Patricia	Mellado Acevedo	006	1
+7	Rene	Peña Aguilar	007	1
+8	Héctor Manuel	Pincheira Conejeros	008	1
+9	Sara	Rojas Aldea	009	1
+10	Marta	Rojas Estay	010	1
+11	Maria Victoria	Vallejos Amado	011	1
+13	Alejandro	Reyes	013	1
+14	Sergio	Muñoz	014	1
+15	Jorge	Pavez	015	1
+16	Jorge	Morris	016	1
+17	Rene	Peña	017	1
+12	Santiago	Zapata Caceres	17.680.010-0	1
+\.
 
 
 --
@@ -557,8 +646,10 @@ SELECT pg_catalog.setval('docentes_pk_seq', 1, false);
 -- Data for Name: escuelas; Type: TABLE DATA; Schema: public; Owner: postgres
 --
 
-INSERT INTO escuelas VALUES (1, 1, 'Informatica', 'Carreras ingenieria en informatica y civil en computacion');
-INSERT INTO escuelas VALUES (2, 2, 'Industria y Civil industrial', '');
+COPY escuelas (pk, departamento_fk, escuela, descripcion) FROM stdin;
+1	1	Informatica	Carreras ingenieria en informatica y civil en computacion
+2	2	Industria y Civil industrial	
+\.
 
 
 --
@@ -572,8 +663,10 @@ SELECT pg_catalog.setval('escuelas_pk_seq', 1, false);
 -- Data for Name: facultades; Type: TABLE DATA; Schema: public; Owner: postgres
 --
 
-INSERT INTO facultades VALUES (1, 'Ingenieria', 'Macul');
-INSERT INTO facultades VALUES (2, 'FAE', 'nose');
+COPY facultades (pk, facultad, descripcion) FROM stdin;
+1	Ingenieria	Macul
+2	FAE	nose
+\.
 
 
 --
@@ -587,15 +680,17 @@ SELECT pg_catalog.setval('facultades_pk_seq', 3, true);
 -- Data for Name: periodos; Type: TABLE DATA; Schema: public; Owner: postgres
 --
 
-INSERT INTO periodos VALUES (1, 1, '1', '08:15:00', '09:35:00');
-INSERT INTO periodos VALUES (2, 2, '2', '09:45:00', '11:05:00');
-INSERT INTO periodos VALUES (3, 3, '3', '11:15:00', '12:35:00');
-INSERT INTO periodos VALUES (4, 4, '4', '12:45:00', '14:05:00');
-INSERT INTO periodos VALUES (5, 5, '5', '14:15:00', '15:35:00');
-INSERT INTO periodos VALUES (6, 6, '6', '15:45:00', '17:05:00');
-INSERT INTO periodos VALUES (7, 7, '7', '17:15:00', '18:35:00');
-INSERT INTO periodos VALUES (8, 8, '8', '19:00:00', '20:30:00');
-INSERT INTO periodos VALUES (9, 9, '9', '20:45:00', '22:15:00');
+COPY periodos (pk, numero, periodo, inicio, termino) FROM stdin;
+1	1	1	08:00:00	09:30:00
+2	2	2	09:40:00	11:10:00
+4	4	4	13:00:00	14:30:00
+5	5	5	14:40:00	16:10:00
+6	6	6	16:20:00	17:50:00
+7	7	7	18:00:00	19:30:00
+8	8	8	19:00:00	20:30:00
+9	9	9	20:40:00	22:10:00
+3	3	3	11:20:00	12:50:00
+\.
 
 
 --
@@ -609,7 +704,9 @@ SELECT pg_catalog.setval('periodos_pk_seq', 1, false);
 -- Data for Name: reservas; Type: TABLE DATA; Schema: public; Owner: postgres
 --
 
-INSERT INTO reservas VALUES (1, '2013-12-20', 15, 6, 1, 1);
+COPY reservas (pk, fecha, sala_fk, periodo_fk, curso_fk, adm_fk) FROM stdin;
+1	2013-12-20	15	6	1	1
+\.
 
 
 --
@@ -623,52 +720,54 @@ SELECT pg_catalog.setval('reservas_pk_seq', 1, true);
 -- Data for Name: salas; Type: TABLE DATA; Schema: public; Owner: postgres
 --
 
-INSERT INTO salas VALUES (1, 1, 'M1-301');
-INSERT INTO salas VALUES (2, 1, 'M1-302');
-INSERT INTO salas VALUES (3, 1, 'M1-303');
-INSERT INTO salas VALUES (4, 1, 'M1-304');
-INSERT INTO salas VALUES (5, 1, 'M1-305');
-INSERT INTO salas VALUES (6, 1, 'M1-306');
-INSERT INTO salas VALUES (7, 1, 'M1-307');
-INSERT INTO salas VALUES (8, 1, 'M2-201');
-INSERT INTO salas VALUES (9, 1, 'M2-202');
-INSERT INTO salas VALUES (10, 1, 'M2-203');
-INSERT INTO salas VALUES (11, 1, 'M2-204');
-INSERT INTO salas VALUES (12, 1, 'M2-301');
-INSERT INTO salas VALUES (13, 1, 'M2-302');
-INSERT INTO salas VALUES (14, 1, 'M2-303');
-INSERT INTO salas VALUES (15, 1, 'M2-304');
-INSERT INTO salas VALUES (16, 1, 'M3-101');
-INSERT INTO salas VALUES (17, 1, 'M3-102');
-INSERT INTO salas VALUES (18, 1, 'M3-103');
-INSERT INTO salas VALUES (19, 1, 'M3-104');
-INSERT INTO salas VALUES (20, 1, 'M3-201');
-INSERT INTO salas VALUES (21, 1, 'M3-202');
-INSERT INTO salas VALUES (22, 1, 'M3-203');
-INSERT INTO salas VALUES (23, 1, 'M3-204');
-INSERT INTO salas VALUES (24, 1, 'M3-301');
-INSERT INTO salas VALUES (25, 1, 'M3-303');
-INSERT INTO salas VALUES (26, 1, 'M3-304');
-INSERT INTO salas VALUES (27, 1, 'M3-400');
-INSERT INTO salas VALUES (28, 1, 'M3-402');
-INSERT INTO salas VALUES (29, 1, 'M6-205');
-INSERT INTO salas VALUES (30, 1, 'M6-206');
-INSERT INTO salas VALUES (31, 1, 'M6-209');
-INSERT INTO salas VALUES (32, 1, 'M6-210');
-INSERT INTO salas VALUES (33, 1, 'M6-212');
-INSERT INTO salas VALUES (34, 1, 'M6-214');
-INSERT INTO salas VALUES (35, 1, 'M6-325');
-INSERT INTO salas VALUES (36, 1, 'M6-326');
-INSERT INTO salas VALUES (37, 1, 'M6-327');
-INSERT INTO salas VALUES (38, 1, 'M6-330');
-INSERT INTO salas VALUES (39, 1, 'M6-331');
+COPY salas (pk, facultad_fk, sala) FROM stdin;
+1	1	M1-301
+2	1	M1-302
+3	1	M1-303
+4	1	M1-304
+5	1	M1-305
+6	1	M1-306
+7	1	M1-307
+8	1	M2-201
+9	1	M2-202
+10	1	M2-203
+11	1	M2-204
+12	1	M2-301
+13	1	M2-302
+14	1	M2-303
+15	1	M2-304
+16	1	M3-101
+17	1	M3-102
+18	1	M3-103
+19	1	M3-104
+20	1	M3-201
+21	1	M3-202
+22	1	M3-203
+23	1	M3-204
+24	1	M3-301
+25	1	M3-303
+26	1	M3-304
+27	1	M3-400
+28	1	M3-402
+29	1	M6-205
+30	1	M6-206
+31	1	M6-209
+32	1	M6-210
+33	1	M6-212
+34	1	M6-214
+35	1	M6-325
+36	1	M6-326
+37	1	M6-327
+38	1	M6-330
+39	1	M6-331
+\.
 
 
 --
 -- Name: salas_pk_seq; Type: SEQUENCE SET; Schema: public; Owner: postgres
 --
 
-SELECT pg_catalog.setval('salas_pk_seq', 1, false);
+SELECT pg_catalog.setval('salas_pk_seq', 40, true);
 
 
 --
@@ -685,6 +784,38 @@ ALTER TABLE ONLY administrador
 
 ALTER TABLE ONLY asignaturas
     ADD CONSTRAINT asignaturas_pkey PRIMARY KEY (pk);
+
+
+--
+-- Name: asistencia_docente_fk_key; Type: CONSTRAINT; Schema: public; Owner: postgres; Tablespace: 
+--
+
+ALTER TABLE ONLY asistencia
+    ADD CONSTRAINT asistencia_docente_fk_key UNIQUE (docente_fk);
+
+
+--
+-- Name: asistencia_pkey; Type: CONSTRAINT; Schema: public; Owner: postgres; Tablespace: 
+--
+
+ALTER TABLE ONLY asistencia
+    ADD CONSTRAINT asistencia_pkey PRIMARY KEY (pk);
+
+
+--
+-- Name: bitacora_administrador_fk_key; Type: CONSTRAINT; Schema: public; Owner: postgres; Tablespace: 
+--
+
+ALTER TABLE ONLY bitacora
+    ADD CONSTRAINT bitacora_administrador_fk_key UNIQUE (administrador_fk);
+
+
+--
+-- Name: bitacora_pkey; Type: CONSTRAINT; Schema: public; Owner: postgres; Tablespace: 
+--
+
+ALTER TABLE ONLY bitacora
+    ADD CONSTRAINT bitacora_pkey PRIMARY KEY (pk);
 
 
 --
@@ -817,6 +948,22 @@ CREATE UNIQUE INDEX salas_facultad_fk_sala_key ON salas USING btree (facultad_fk
 
 ALTER TABLE ONLY asignaturas
     ADD CONSTRAINT asignaturas_departamento_fk_fkey FOREIGN KEY (departamento_fk) REFERENCES departamentos(pk) ON UPDATE CASCADE ON DELETE CASCADE;
+
+
+--
+-- Name: asistencia_docente_fk_fkey; Type: FK CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY asistencia
+    ADD CONSTRAINT asistencia_docente_fk_fkey FOREIGN KEY (docente_fk) REFERENCES docentes(pk) ON UPDATE CASCADE ON DELETE CASCADE;
+
+
+--
+-- Name: bitacora_administrador_fk_fkey; Type: FK CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY bitacora
+    ADD CONSTRAINT bitacora_administrador_fk_fkey FOREIGN KEY (administrador_fk) REFERENCES administrador(pk) ON UPDATE CASCADE ON DELETE CASCADE;
 
 
 --
